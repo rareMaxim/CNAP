00c2f045839d0626cdcd3639a4b809027ff6d067
rareMax <maks4a@gmail.com>
2023-06-11 23:07:19 +0300

    Додано налаштування лого на стартовій сторінці

--- cnap/www/cnap-start/index.py
+++ cnap/www/cnap-start/index.py
@@ -3,5 +3,9 @@ import frappe
 def get_context(context):
     them_areas = frappe.db.get_list('CNAP Thematic area',
                                     fields=['name', 'description'],)
+    config = frappe.get_doc('Cnap Config') 
+    print(config.title)    
+    context.config = config                
     context.them_areas = them_areas
+
     return context 
