6b4ad223efe56360741bd0dfa7454048c37e992f
rareMax <maks4a@gmail.com>
2023-06-11 17:09:01 +0300

    work with Portal

--- cnap/cnap/doctype/cnap_applicant_type/cnap_applicant_type.js
+++ cnap/cnap/doctype/cnap_applicant_type/cnap_applicant_type.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Maxim Sysoev and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("CNAP Documents", {
+// frappe.ui.form.on("Cnap Applicant Type", {
 // 	refresh(frm) {
 
 // 	},
--- cnap/cnap/doctype/cnap_service_provider/test_cnap_service_provider.py
+++ cnap/cnap/doctype/cnap_service_provider/test_cnap_service_provider.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class Testtest(FrappeTestCase):
+class TestCnapServiceProvider(FrappeTestCase):
 	pass
