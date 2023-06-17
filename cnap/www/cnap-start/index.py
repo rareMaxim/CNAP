131130a405f6c4b5a19227fe8bd14d7723c80b48
rareMax <maks4a@gmail.com>
2023-06-17 23:41:31 +0300

    Додано можливість вказувати посилання на соціальні мережі

--- cnap/www/cnap-start/index.py
+++ cnap/www/cnap-start/index.py
@@ -3,8 +3,7 @@ import frappe
 def get_context(context):
     them_areas = frappe.db.get_list('CNAP Thematic area',
                                     fields=['name', 'description'],)
-    config = frappe.get_doc('Cnap Config') 
-    print(config.title)    
+    config = frappe.get_doc('Cnap Config')
     context.config = config                
     context.them_areas = them_areas
 
